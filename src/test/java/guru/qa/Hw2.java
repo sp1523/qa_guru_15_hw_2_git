2502ebc73c5aa0d50209761e9a8859bc16f41d8f
sp1523 <theftorm@gmail.com>
2022-09-18 01:06:41 +0300

    another conflict try

--- src/test/java/guru/qa/Hw2.java
+++ src/test/java/guru/qa/Hw2.java
@@ -12,9 +12,8 @@ public class Hw2 {
 
     @Test
     void Hw2() {
-        Configuration.browser = FIREFOX;
 
-         open("https://www.google.com/");
+        open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
