98a94fc08b944c6e080c3b1dc8cbda0fe507a017
Погодин Сергей <spogodin@sberhealth.ru>
2022-09-18 00:10:54 +0300

    first commit from console

---
+++ src/test/java/guru/qa/Hw2.java
@@ -0,0 +1,20 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.module.Configuration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Hw2 {
+
+    @Test
+    void Hw2() {
+
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
